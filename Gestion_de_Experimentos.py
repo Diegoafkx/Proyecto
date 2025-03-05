626f98b6ad4ed07c22877a609cb07c17e4d441a2
diegox <diego.arreaza@correo.unimet.edu.ve>
2025-03-05 12:51:00 -0400

    Update Gestion_de_Experimentos.py

--- Gestion_de_Experimentos.py
+++ Gestion_de_Experimentos.py
@@ -5,3 +5,16 @@ class Gestion_de_Experimentos(__Experimnto,__Receta):
     def __init__(self):
         super().__init__()
         Gestion_de_Experimentos.Lector_de_Datos(self)
+        
+    def Analizador_de_Informacion_Recetas(self,indicador_de_receta,accion):
+        self._id_receta = None
+        self._nombre = None
+        self._objetivo = None
+        self._reactivos_utilizados = []
+        self._procedimiento = []
+        self._valores_a_medir = []
+        self._receta = []
+    
+    
+    def Analizador_de_Informacion_Experimento(self,indicador_de_experimento,accion):
+           
